fc3ae8baecae29849caf324c511bf1a5ef33e713
s14003 <s14003@std.it-college.ac.jp>
2016-01-31 16:38:00 +0900

    色々変更

---
+++ app/src/main/java/jp/ac/it_college/std/s14003/android/bluetoothchat/Constants.java
@@ -0,0 +1,13 @@
+package jp.ac.it_college.std.s14003.android.bluetoothchat;
+
+public interface Constants {
+    int MESSAGE_STATE_CHANGE = 1;
+    int MESSAGE_READ = 2;
+    int MESSAGE_WRITE = 3;
+    int MESSAGE_DEVICE_NAME = 4;
+    int MESSAGE_TOAST = 5;
+
+    // Key names received from the BluetoothChatService Handler
+    String DEVICE_NAME = "device_name";
+    String TOAST = "toast";
+}
